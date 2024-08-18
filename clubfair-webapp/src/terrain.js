ac8e898b7d19c6c386d5d1ef73d813c7e940f78e
mharhay213 <151762557+mharhay213@users.noreply.github.com>
2024-08-17 20:56:44 -0400

    Added colour to grid points & added varying z positions using noise. Added orbit controls.

--- clubfair-webapp/src/terrain.js
+++ clubfair-webapp/src/terrain.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { ImprovedNoise } from 'three/examples/jsm/Addons.js';
 
 const Terrain = () => {
     // Init scene & camera
@@ -14,29 +16,48 @@ const Terrain = () => {
     renderer.setAnimationLoop(animate);
     document.body.appendChild(renderer.domElement);
 
+    // Orbit controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.update();
+
     // Generate points
     const edgeSize = 20;
     const offset = {x: -2.25, y: -2.25};
     const gap = 0.25;
-    const pointSize = 0.3;
-    const pointColor = 0x00ff00;
+    const pointSize = 0.33;
+
+    const noise = new ImprovedNoise();
     const grid = [];
-    let vertex;
+    const colors = [];
+    let ns;
+    //let vertex;
 
     for (let row = 0; row < edgeSize; row++) {
         for (let col = 0; col < edgeSize; col++) {
+            
+            // Generate z coord with noise
             let x = offset.x + col * gap;
             let y = offset.y + row * gap;
             let z = 0;
+            ns = noise.noise(x, y, 0);  
+            z = ns;    
 
-            vertex = new THREE.Vector3(x, y, z);
+            // Pick colors
+            let r = Math.random();
+            let g = Math.random();
+            let b = Math.random();
+
+            //vertex = new THREE.Vector3(x, y, z);
             grid.push(x, y, z);
+            colors.push(r, g, b);
         }
     }
 
     const geometry = new THREE.BufferGeometry();
     geometry.setAttribute("position", new THREE.Float32BufferAttribute(grid, 3));
-    const material = new THREE.PointsMaterial({size: pointSize, color: pointColor});
+    geometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
+    const material = new THREE.PointsMaterial({size: pointSize, vertexColors: true});
     const points = new THREE.Points(geometry, material);
     scene.add(points);
 
